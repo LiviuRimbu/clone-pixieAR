080d61484257e85e61eda963da9e602aad7033e3
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2024-12-22 11:34:45 +0200

    Deleted sensitive info

--- src/data/books-list.ts
+++ src/data/books-list.ts
@@ -6,7 +6,7 @@ export interface books {
 }
 
 export const books: books[] = [
-	{ id: 0, src: 'https://mywebar.com/', imageSrc: '/images/english.png' },
-	{ id: 1, src: 'https://mywebar.com/', imageSrc: '/images/grammar.png' },
-	{ id: 2, src: 'https://mywebar.com/', imageSrc: '/images/france.png' },
+	{ id: 0, src: 'https://mywebar.com/1', imageSrc: '/images/english.png' },
+	{ id: 1, src: 'https://mywebar.com/2', imageSrc: '/images/grammar.png' },
+	{ id: 2, src: 'https://mywebar.com/3', imageSrc: '/images/france.png' },
 ];
